02ef1eb3bacb64bbd9722890bc7ace7effd90d1c
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2025-03-10 23:28:48 +0100

    +layout setup
    - added all the necessary components
    - styled the layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	let {children} = $props();
+	import {Header, Footer, Window,Menu} from '$lib';
+	// import '../app.css' with { type: "css" };
+	
+</script>
+
+<Window role='window' >
+	<Header />
+	<Menu />
+
+	{@render children()}
+	
+</Window>
+
+<Footer class='ftr'/>
+
+
+<style>
+
+	:root{
+		--H-top:5cqh;
+		--Padding-genral:2cqh;
+	}
+
+	:global(body){
+		display: flex;
+		flex-direction: column;
+		height: 100dvh;
+		overflow-y: hidden;
+    	overflow-x: hidden;
+
+
+	}
+
+
+	:global(header){
+		padding-inline: var(--Padding-genral);
+		padding-block: .5%;
+	}
+
+
+	:global(main){
+		flex: 1 !important;
+		display: flex;
+		flex-direction: column;
+		margin: 0;
+		padding: 0;
+		overflow-y: hidden;
+    	overflow-x: hidden;
+
+	}
+
+
+	:global(main.contain){
+		background-color: rgba(213, 213, 213, 0.904) ;
+	}
+
+	:global(.mainContain){
+		/* padding-inline: var(--Padding-genral); */
+		/* margin: var(--Padding-genral); */
+
+	}
+
+	:global(footer){
+		flex: 0 1 auto;
+		padding-inline: var(--Padding-genral);
+
+	}
+
+</style>
