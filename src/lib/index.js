09d326032713d66c8040863b19ce81cc174e9712
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2025-03-10 23:13:04 +0100

    general
    added
    - icon photo
    - global css
    - components routes in lib
    
    updated
    - package json

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,11 @@
 // place files you want to import through the `$lib` alias in this folder.
+export { default as Buttons } from './atoms/Buttons.svelte';
+
+export { default as Menu } from './molecule/Menu.svelte';
+export { default as Footer } from './molecule/Footer.svelte';
+
+export { default as Header } from './system/Header.svelte';
+export {default as Window} from './system/Window.svelte';
+
+
+
