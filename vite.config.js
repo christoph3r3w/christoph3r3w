2f13a32597495a3664b91cb53ba8e7a1d2932818
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2026-05-11 23:14:38 +0200

    System updates
    - General system updates
    - Added new Image plugin To vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		enhancedImages()
+	],
 	server: {
+		host:true,
 		hmr: {
 			overlay: false
 		}
